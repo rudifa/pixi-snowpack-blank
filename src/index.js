aead001fcf66b91a602775e6c14606692f74375e
Rudi Farkas <rudi.farkas@gmail.com>
2020-12-15 18:12:17 +0100

    add keyboard events

--- src/index.js
+++ src/index.js
@@ -1,28 +1,43 @@
-import { Application } from '@pixi/app'; // WORKS
-// import { Application } from 'pixi.js'; // DOES NOT WORK
-import { Graphics } from '@pixi/graphics';
-import { Renderer } from '@pixi/core'; // Renderer is the class that is going to register plugins
+// -------------------
+// import PIXI classes
+// -------------------
 
-import { BatchRenderer } from '@pixi/core'; // BatchRenderer is the "plugin" for drawing sprites
+import { Application } from '@pixi/app';
+
+import { Graphics } from '@pixi/graphics';
+
+// Renderer is the class that registers plugins
+import { Renderer } from '@pixi/core';
+
+// BatchRenderer is the plugin for drawing sprites
+import { BatchRenderer } from '@pixi/core';
 Renderer.registerPlugin('batch', BatchRenderer);
 
-import { TickerPlugin } from '@pixi/ticker'; // TickerPlugin is the plugin for running an update loop (it's for the application class)
+// TickerPlugin is the plugin for running an update loop (it's for the application class)
+import { TickerPlugin } from '@pixi/ticker';
 Application.registerPlugin(TickerPlugin);
 
+// InteractionManager handles mouse events
 import { InteractionManager } from '@pixi/interaction';
-
 Renderer.registerPlugin('interaction', InteractionManager);
-// // And just for convenience let's register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
+
+// Just for convenience let's register Loader plugin in order to use it right from Application instance like app.loader.add(..) etc.
 // import { AppLoaderPlugin } from '@pixi/loaders';
 // Application.registerPlugin(AppLoaderPlugin);
 
-// Sprite is our image on the stage
-import { Sprite } from '@pixi/sprite';
-
+// ------------------------------------
+// create Application and connect it up
+// ------------------------------------
 const app = new Application({
-  width: 600,
-  height: 600,
-  backgroundColor: 0x1099bb,
+  width: 700,
+  height: 700,
+  backgroundColor: 0x304050,
+});
+
+document.body.appendChild(app.view);
+
+app.renderer.view.addEventListener('click', function (e) {
+  console.log('click', e);
 });
 
 console.log('app', app);
@@ -31,36 +46,16 @@ console.log('app.stage', app.stage, app.stage.width);
 console.log('app.renderer', app.renderer, app.renderer.width);
 console.log('app.renderer.plugins', app.renderer.plugins);
 
-// create a manager instance, passing stage and renderer.view
-// var manager = new InteractionManager(app.stage, app.renderer.view);
-//app.renderer.registerPlugin('interaction', InteractionManager);
-document.body.appendChild(app.view);
+// Sprite is our image on the stage
+import { Sprite } from '@pixi/sprite';
 
-app.renderer.view.addEventListener('click', function (e) {
-  console.log(e);
-});
-
-// not interacitive!
+// app.stage is not interacitive!
 // app.stage.on('pointerdown', onTouchDown);
 // app.stage.interactive = true;
 
-// code from https://codepen.io/rudifa/pen/qBarNLg
-
-// var canv = document.getElementById('canvas');
-// canv.width = 500;
-// canv.height = 500;
-
-// var ctx = canv.getContext('2d');
-// ctx.rect(0, 0, canv.width, canv.height);
-// ctx.fillStyle = "#888888"
-// ctx.fill();
-
-// const renderer = PIXI.autoDetectRenderer(256, 256);
-// document.body.appendChild(renderer.view);
-
-// const stage = new PIXI.Container();
-// renderer.render(stage);
-
+// -----------------------------------------
+// TRY CIRCLE
+// -----------------------------------------
 function makeCircle(x, y, r, fillcolor) {
   var graphics = new Graphics();
   graphics.beginFill(fillcolor);
@@ -76,6 +71,11 @@ function makeCircle(x, y, r, fillcolor) {
   return graphics;
 }
 app.stage.addChild(makeCircle(160, 285, 60, 0x874cac));
+
+// ----------------------------------------
+// DRAGGABLE HEXAGONS
+// from https://codepen.io/zeakd/pen/NdMBgB
+// ----------------------------------------
 
 var hexagonRadius = 60;
 var hexagonHeight = hexagonRadius * Math.sqrt(3);
@@ -182,3 +182,130 @@ function toHexagonPosition(p) {
   console.log('toHexagonPosition', p, newP);
   return newP;
 }
+
+// -------------------------------------------------
+// KEYBOARD EVENTS
+// from https://codepen.io/SkuliOskarsson/pen/ZbJKVW
+// -------------------------------------------------
+//(function () {
+// Set up Pixi.js
+// var renderer = PIXI.autoDetectRenderer(660, 660, {
+//   backgroundColor: 0x34495e,
+//   antialias: true,
+// });
+// document.body.appendChild(renderer.view);
+
+// // Create the stage
+// var stage = new PIXI.Container();
+
+// Set the width and height of our boxes
+var boxWidth = app.renderer.width / 10;
+var boxHeight = app.renderer.height / 10;
+
+// Create the "player"
+var playerBox = new Graphics();
+playerBox.lineStyle(1, 0x000000);
+playerBox.beginFill(0x3498dff); // Blue color
+playerBox.drawRect(0, 0, boxWidth, boxHeight);
+playerBox.endFill();
+
+// Create the target
+var goalBox = new Graphics();
+goalBox.lineStyle(1, 0x000000);
+goalBox.beginFill(0xe74c3c); // Red color
+goalBox.drawRect(0, 0, boxWidth, boxHeight);
+goalBox.endFill();
+
+// Add boxes to the stage
+app.stage.addChild(playerBox);
+app.stage.addChild(goalBox);
+
+// Add the 'keydown' event listener to our document
+//document.addEventListener('keydown', onKeyDown);
+
+// Spawn our target
+goalBoxSpawn();
+
+animate();
+
+function animate() {
+  // Render the stage
+  app.renderer.render(app.stage);
+
+  // Check if your player collides with the target
+  checkPosition();
+  requestAnimationFrame(animate);
+}
+
+function goalBoxSpawn() {
+  // Spawns the target at a random position on our stage
+
+  // Create two random points on our stage
+  var randomX = Math.floor(Math.random() * 10 + 0);
+  var randomY = Math.floor(Math.random() * 10 + 0);
+
+  // Set the position of our target
+  goalBox.position.x = boxWidth * randomX;
+  goalBox.position.y = boxHeight * randomY;
+}
+
+function checkPosition() {
+  // If the player and target are at the same position, spawn the target in another position
+  if (
+    goalBox.position.x === playerBox.position.x &&
+    goalBox.position.y === playerBox.position.y
+  ) {
+    goalBoxSpawn();
+  }
+}
+
+function onKeyDown(key) {
+  // W Key is 87
+  // Up arrow is 87
+  if (key.keyCode === 87 || key.keyCode === 38) {
+    // If the W key or the Up arrow is pressed, move the player up.
+    if (playerBox.position.y != 0) {
+      // Don't move up if the player is at the top of the stage
+      playerBox.position.y -= boxHeight;
+    }
+  }
+
+  // S Key is 83
+  // Down arrow is 40
+  if (key.keyCode === 83 || key.keyCode === 40) {
+    // If the S key or the Down arrow is pressed, move the player down.
+    if (playerBox.position.y != app.renderer.height - boxHeight) {
+      // Don't move down if the player is at the bottom of the stage
+      playerBox.position.y += boxHeight;
+    }
+  }
+
+  // A Key is 65
+  // Left arrow is 37
+  if (key.keyCode === 65 || key.keyCode === 37) {
+    // If the A key or the Left arrow is pressed, move the player to the left.
+    if (playerBox.position.x != 0) {
+      // Don't move to the left if the player is at the left side of the stage
+      playerBox.position.x -= boxWidth;
+    }
+  }
+
+  // D Key is 68
+  // Right arrow is 39
+  if (key.keyCode === 68 || key.keyCode === 39) {
+    // If the D key or the Right arrow is pressed, move the player to the right.
+    if (playerBox.position.x != app.renderer.width - boxWidth) {
+      // Don't move to the right if the player is at the right side of the stage
+      playerBox.position.x += boxWidth;
+    }
+  }
+}
+//})();
+
+// Added this to codepen only so that the blog readers can try it in the iframe.
+// var playButton = document.getElementById('play-button');
+// playButton.addEventListener('click', function () {
+//   var preview = document.getElementById('preview');
+
+//   preview.setAttribute('class', 'hide');
+// });
